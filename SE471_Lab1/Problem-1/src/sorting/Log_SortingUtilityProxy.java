a630a9f1152e052390e3c80518ea1e9e73b8cd16
beaudougan1 <douga002@csusm.edu>
2025-02-13 19:19:00 -0800

    Update Log_SortingUtilityProxy.java

--- SE471_Lab1/Problem-1/src/sorting/Log_SortingUtilityProxy.java
+++ SE471_Lab1/Problem-1/src/sorting/Log_SortingUtilityProxy.java
@@ -2,7 +2,7 @@ package sorting;
 import model.Product;
 import java.util.List;
 
-public class Log_SortingUtilityProxy {
+public class Log_SortingUtilityProxy extends SortingUtility {
     // Will list the sorted products before returning to client
 
     // Bubble Sort returns in order: ID, Name, & Price
@@ -11,9 +11,23 @@ public class Log_SortingUtilityProxy {
     // Quick Sort returns in order: Name, ID, & Price
     //...
 
-    public static void logProducts(List<Product> items){
+    public Log_SortingUtilityProxy(List<Product> items) {
+        System.out.println("Bubblesort");
+        List<Product> bubbleItems = super.sort(items, 1);
+        logProducts(items, 1);
+        System.out.println("Quicksort:");
+        List<Product> quickItems = super.sort(items, 2);
+        logProducts(items, 2);
+    }
+
+    public static void logProducts(List<Product> items, int sortingApproach){
         for(Product p : items){
-            System.out.println("Name: " + p.getName() + " ID: " + p.getId() + " Price: " + p.getPrice());
+            if(sortingApproach == 1){
+                System.out.println("ID: " + p.getId() + " Name: " + p.getName() + " Price: " + p.getPrice());
+            }
+            else if(sortingApproach == 2){
+                System.out.println("Name: " + p.getName() + " ID: " + p.getId() + " Price: " + p.getPrice());
+            }
         }
     }
 }
